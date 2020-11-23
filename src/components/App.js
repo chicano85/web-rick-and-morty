fe35a0b6786ca2fe6467f9033766f677ce7f058d
chicano85 <chicano.cano@gmail.com>
2020-11-23 20:36:23 +0100

    add useEffect and useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
+import React, { useEffect, useState } from "react";
 import "../App.css";
+import getDataFromApi from "../services/api";
 
-function App() {
+const App = () => {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    // guardar en el estado los datos de la api
+    getDataFromApi().then((data) => {
+      setCharacters(data);
+    });
+  });
   return <div className="App">hola mundo</div>;
-}
+};
 
 export default App;
